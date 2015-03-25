a4a7bb5bd33b988b5e444f8ddf7458cd18bc3200
Luisa Mckenna <mckennal1@wit.edu>
2015-03-25 13:13:18 -0400

    lab 5 done!

--- labs/lab5/model.cpp
+++ labs/lab5/model.cpp
@@ -36,27 +36,43 @@ void Model::go(Direction d) {
 // Move foward
 void Model::crawl() {
     Coordinate front = snake.front();
+
+
+    // TODO: Colliding with food grows the snake (so don't pop_back in that case)/ done
+    if(front.x == food.x && front.y == food.y){
+        Coordinate back = snake.back();
+        snake.push_back(back);
+        food.x = rand()%width;
+        food.y = rand()%height;
+    }
+    else {
+        snake.pop_back();
+    }
+
+    // detect collision
+    // set direction = DEAD
+    // TODO: Colliding with the perimeter of the screen should set direction to DEAD
+    // When DEAD, the snake slowly shrinks down to nothing
+    if (front.x < 0 || front.x == width || front.y < 0 || front.y == height){
+        direction = DEAD;
+        snake.pop_back();
+    }
+    // TODO: Also, colliding with food should cause us to place the food somewhere
+    // else, but not anywhere on the snake.
+    for (std::list<Coordinate>::iterator it=(++snake.begin()); it!=snake.end(); it++) {
+        if (front.x == it->x && front.y == it->y) {
+            direction = DEAD;
+        }
+    }
+
     switch(direction) {
     case UP: front.y--; break;
     case DOWN: front.y++; break;
     case LEFT: front.x--; break;
     case RIGHT: front.x++; break;
+    case DEAD: break;
     }
-    // TODO: Colliding with the perimeter of the screen should set direction to DEAD
-    // When DEAD, the snake slowly shrinks down to nothing
     if (direction != DEAD) {
         snake.push_front(front);
     }
-
-    // TODO: Colliding with food grows the snake (so don't pop_back in that case)
-    if(head.x == food.x && head.y == food.y){
-        Coordinate back = snake.back();
-        back.x = back.x + 1;
-        snake.push_front(back);
-    }
-    else {
-        snake.pop_back();
-    }
-    // TODO: Also, colliding with food should cause us to place the food somewhere
-    // else, but not anywhere on the snake.
 }
